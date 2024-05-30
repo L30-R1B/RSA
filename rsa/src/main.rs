94d4fb803f219f5db1fe406a4d9f123131c75c3f
LeonardoRibeiro <lr20250523@gmail.com>
2024-05-30 12:03:28 -0300

    Alteração da função main

--- rsa/src/main.rs
+++ rsa/src/main.rs
@@ -7,19 +7,6 @@ mod aux;
 mod compactar;
 
 fn main() {
-    let mut total_duration = Duration::new(0, 0);
-    let iterations = 1;
-
-    for _ in 0..iterations {
-        let start = Instant::now();
-        
-        let p = script::Processo::new("init");
-        script::Processo::realiza_processo(p);
-
-        let duration = start.elapsed();
-        total_duration += duration;
-    }
-
-    let average_duration = total_duration / iterations as u32;
-    println!("Tempo médio de execução: {:?}", average_duration);
+    let p = script::Processo::new("init");
+    script::Processo::realiza_processo(p);
 }
